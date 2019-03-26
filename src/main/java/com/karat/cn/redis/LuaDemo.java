2c68b2f0af630cd116e1902f4ed231498f850d86
21karat <1252515809@qq.com>
2019-03-26 16:40:14 +0800

    修改

--- src/main/java/com/karat/cn/redis/LuaDemo.java
+++ src/main/java/com/karat/cn/redis/LuaDemo.java
@@ -15,7 +15,9 @@ public class LuaDemo {
 			String lua="redis.call('set','name','xiaoming')\n"
 					+ "return redis.call('get','name')";//脚本内容
 			
-			Object object=jedis.eval(lua);
+			/*Object object=jedis.eval(lua);
+			System.out.println(object);*/
+			Object object=jedis.evalsha(jedis.scriptLoad(lua));//scriptLoad得到摘要，根据摘要得到脚本
 			System.out.println(object);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
